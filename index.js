498a53218bd9d54e1b536cd7af56ac5bba5286db
Kadir Gün <kadir@kadirgun.com>
2018-08-03 07:44:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var proxy = require('express-http-proxy');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(proxy('https://ipinfo.io'));
+app.use(proxy('https://cron.buysocial.net'));
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
