1184862a146acab7dcce2fd5d2d7bb2d9fb348af
Kadir Gün <kadir@kadirgun.com>
2018-08-03 08:23:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var proxy = require('express-http-proxy');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(proxy('https://cron.buysocial.net'),{
-  proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
-    delete proxyReqOpts.headers['X-Forwarded-For'];
-    return proxyReqOpts;
-  }
-});
+app.use(proxy('https://cron.buysocial.net',{
+    proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
+      delete proxyReqOpts.headers['X-Forwarded-For'];
+      return proxyReqOpts;
+    }
+  })
+);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
