a107834e769f8db6985f3829832a544f25cefde1
Kadir Gün <kadir@kadirgun.com>
2018-08-03 09:31:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ var app = express()
 app.set('trust proxy', true);
 
 app.set('port', (process.env.PORT || 5000))
-app.use(proxy('http://fese.win',{
-    proxyReqOptDecorator: function(proxyReqOpts, srcReq) {
-      delete proxyReqOpts.headers['X-Forwarded-For'];
-      return proxyReqOpts;
-    }
-  })
-);
+app.use(proxy('https://www.instagram.com'));
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
