acad1e3f40c7c479439317defc7b9db21ca6ce57
Kadir Gün <kadir@kadirgun.com>
2018-08-03 07:24:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var proxy = require('express-http-proxy');
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(proxy('https://www.instagram.com'));
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
